7f2f7d3c355c366104b5cb7a6509655fb87aafe0
RodyZack07 <wildanad466@gmail.com>
2026-04-05 18:57:54 +0700

    Minor Update

--- Middleware/ExceptionHandlerExtension.cs
+++ Middleware/ExceptionHandlerExtension.cs
@@ -1,6 +1,37 @@
-﻿namespace reminder_schedule_backend.Middleware
+﻿using Microsoft.AspNetCore.Diagnostics;
+using reminder_schedule_backend.Exceptions;
+
+
+namespace reminder_schedule_backend.Middleware
 {
-    public class ExceptionHandlerExtension
+    public static class ExceptionHandlerExtension
     {
+        public static void UseGlobalExceptionHandler( this WebApplication app)
+        {
+            app.UseExceptionHandler(errorApp =>
+            {
+                errorApp.Run(async context =>
+                {
+                    var ex = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+                    context.Response.StatusCode = ex switch
+                    {
+                        NotFoundException =>404,
+                        BadRequestException => 401,
+                        ConflictException => 409,
+                        UnauthorizedException => 400,
+                       
+                    };
+
+                    context.Response.ContentType = "application/json";
+
+                    await context.Response.WriteAsJsonAsync(new
+                    {
+                        success = false,
+                        message = ex?.Message ?? "Terjadi kesalahan pada server"
+                    });
+                });
+            });
+        }
     }
 }
